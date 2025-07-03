f4dfd917fcfad5711447625b6293c32fad5cdc34
alexarlord-boop <rezerw0.1@yandex.ru>
2025-07-03 09:50:50 +0300

    Add reviewer data fetching and display in TrustScoreUI component

--- trustScoreUI/src/components/TrustScoreUI.vue
+++ trustScoreUI/src/components/TrustScoreUI.vue
@@ -17,31 +17,41 @@ function getApiUrl(path) {
   return `${protocol}//${host}/index.php/${contextPath}/api/v1/${path}`;
 };
 
-
+// authors fetch
 const submissionUrl = computed(() => getApiUrl(`submissions/${props.submissionId}/publications/${props.submissionId}`));
 console.log('submissionUrl:', submissionUrl.value);
-
 const { data: submissionData, fetch: fetchSubmission } = useFetch(submissionUrl, {});
-
+console.log('submissionData:', submissionData);
 
 const authors = ref([]);
 const hasAuthors = computed(() => authors.value.length > 0);
 
 
+// reviewers fetch 
+console.log(window.pkp.currentUser);
+const assignmentUrl = computed(() => getApiUrl(`submissions/${props.submissionId}/`));
+console.log('assignmentUrl:', assignmentUrl.value);
+const { data: assignmentData, fetch: fetchAssignment } = useFetch(assignmentUrl, {});
+console.log('assignmentData:', assignmentData);
+// console.log('reviewers:', assignmentData?.value.reviewAssignments?.value);
+
+
 onMounted(() => {
   if (props.submissionId) {
     fetchSubmission();
+    fetchAssignment();
   }
+  
 });
 
-watch(submissionData, (newVal) => {
-  console.log('submissionData changed:', newVal);
-  const rawAuthors = newVal?.authors;
-  authors.value = Array.isArray(rawAuthors)
-    ? rawAuthors
-    : Object.values(rawAuthors ?? {});
-  console.log('Updated authors:', authors.value);
-});
+// watch(submissionData, (newVal) => {
+//   console.log('submissionData changed:', newVal);
+//   const rawAuthors = newVal?.authors;
+//   authors.value = Array.isArray(rawAuthors)
+//     ? rawAuthors
+//     : Object.values(rawAuthors ?? {});
+//   console.log('Updated authors:', authors.value);
+// });
 
 
 
@@ -51,10 +61,20 @@ watch(submissionData, (newVal) => {
   <div>
     <h3>Authors</h3>
     <p v-if="submissionData && submissionData.authors">
-      <span v-if="hasAuthors">Authors found:</span>
+      
       <ul>
         <li v-for="author in submissionData.authors" :key="author.id">
           {{ author.fullName }} <a href="#">{{ author.email }}</a>
+        </li>
+      </ul>
+    </p>
+    <h3>Reviewers</h3>
+    <p v-if="assignmentData && assignmentData.reviewAssignments">
+      
+      <ul>
+        <li v-for="reviewer in assignmentData.reviewAssignments" :key="reviewer.id">
+          <!-- {{ author.fullName }} <a href="#">{{ author.email }}</a> -->
+            <pre>{{ reviewer.reviewerFullName }}</pre>
         </li>
       </ul>
     </p>
